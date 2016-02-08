811d518c9edd7aab419877ade81166ec2f6a94f3
Lawrence Thai <lthai423@gmail.com>
2016-02-07 20:44:05 -0800

    added pages

--- contact/urls.py
+++ contact/urls.py
@@ -2,12 +2,12 @@ from django.conf.urls import url
 from django.conf import settings
 from django.conf.urls.static import static
 
-app_name = 'community'
+app_name = 'contact'
 
 from . import views
 urlpatterns = [
 
-    url(r'^/tech-employ/$', views.tech, name ='tech'),
+    url(r'^$', views.contact, name ='contact'),
     
 
 ]
