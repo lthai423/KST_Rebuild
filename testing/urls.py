146beedf2c0d10b82817d18114888fa4d39c0ace
lthai423 <lthai423@gmail.com>
2016-04-02 17:29:11 -0700

    everything but pictures and footers

--- testing/urls.py
+++ testing/urls.py
@@ -7,8 +7,20 @@ app_name = 'testing'
 from . import views
 urlpatterns = [
 
-    url(r'^environmental/$', views.environmental, name ='environmental'),
-    url(r'^ballistic/$', views.ballistic, name ='ballistic'),
-    url(r'^jet/$', views.jet, name ='jet'),
+    url(r'^$', views.testing, name ='testing'),
+    url(r'^launch/', views.launch, name ='launch'),
+    url(r'^environment/', views.environment, name ='environment'),
+    url(r'^ballistic/', views.ballistic, name ='ballistic'),
+    url(r'^jet/', views.jet, name ='jet'),
+    url(r'^protection/', views.protection, name ='protection'),
+    url(r'^impact/', views.impact, name ='impact'),
+    url(r'^seismic/', views.seismic, name ='seismic'),
+    url(r'^fluids/', views.fluids, name ='fluids'),
+    url(r'^electromagnetic/', views.electromagnetic, name ='electromagnetic'),
+    url(r'^reliability/', views.reliability, name ='reliability'),
+    url(r'^acoustics/', views.acoustics, name ='acoustics'),
+    url(r'^structural/', views.structural, name ='structural'),
+    url(r'^engine/', views.engine, name ='engine'),
+    url(r'^explosives/', views.explosives, name ='explosives'),
 ]
 
--- testing/views.py
+++ testing/views.py
@@ -1,10 +1,46 @@
 from django.shortcuts import render
 
-def environmental(request):
-	return render(request, 'testing/environmental.html')
+def testing(request):
+	return render(request, 'testing/index.html')
+
+def launch(request):
+	return render(request, 'testing/launch.html')
+
+def environment(request):
+	return render(request, 'testing/test.html')
 
 def ballistic(request):
-	return render(request, 'testing/ballistic.html')
+	return render(request, 'testing/test.html')
 
 def jet(request):
 	return render(request, 'testing/jet.html')
+
+def protection(request):
+	return render(request, 'testing/test.html')
+
+def impact(request):
+	return render(request, 'testing/test.html')
+
+def seismic(request):
+	return render(request, 'testing/test.html')
+
+def fluids(request):
+	return render(request, 'testing/test.html')
+
+def electromagnetic(request):
+	return render(request, 'testing/test.html')
+
+def reliability(request):
+	return render(request, 'testing/test.html')
+
+def acoustics(request):
+	return render(request, 'testing/test.html')
+
+def structural(request):
+	return render(request, 'testing/test.html')
+
+def engine(request):
+	return render(request, 'testing/test.html')
+
+def explosives(request):
+	return render(request, 'testing/test.html')
