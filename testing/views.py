1aa2372848ea40f6ce95e922d10d47745ad10d41
lthai423 <lthai423@gmail.com>
2016-04-08 07:23:14 -0700

    a

--- testing/views.py
+++ testing/views.py
@@ -7,40 +7,40 @@ def launch(request):
 	return render(request, 'testing/launch.html')
 
 def environment(request):
-	return render(request, 'testing/test.html')
+	return render(request, 'testing/environment.html')
 
 def ballistic(request):
-	return render(request, 'testing/test.html')
+	return render(request, 'testing/ballistic.html')
 
 def jet(request):
 	return render(request, 'testing/jet.html')
 
 def protection(request):
-	return render(request, 'testing/test.html')
+	return render(request, 'testing/protection.html')
 
 def impact(request):
-	return render(request, 'testing/test.html')
+	return render(request, 'testing/impact.html')
 
 def seismic(request):
-	return render(request, 'testing/test.html')
+	return render(request, 'testing/seismic.html')
 
 def fluids(request):
-	return render(request, 'testing/test.html')
+	return render(request, 'testing/fluids.html')
 
 def electromagnetic(request):
-	return render(request, 'testing/test.html')
+	return render(request, 'testing/electromagnetic.html')
 
 def reliability(request):
-	return render(request, 'testing/test.html')
+	return render(request, 'testing/reliability.html')
 
 def acoustics(request):
-	return render(request, 'testing/test.html')
+	return render(request, 'testing/acoustics.html')
 
 def structural(request):
-	return render(request, 'testing/test.html')
+	return render(request, 'testing/structural.html')
 
 def engine(request):
-	return render(request, 'testing/test.html')
+	return render(request, 'testing/engine.html')
 
 def explosives(request):
-	return render(request, 'testing/test.html')
+	return render(request, 'testing/explosives.html')
